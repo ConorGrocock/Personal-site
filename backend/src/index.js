d6032a30a401ce50ab834773785d993b600bdd7b
Conor Grocock <grocock.conor@gmail.com>
2019-10-05 21:46:42 +0100

    Added api route to get all projects from database

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,29 +1,18 @@
-'use strict';
+"use strict";
 
-const express = require('express');
-const path = require('path');
-const mongoose = require("mongoose");
-const cors = require('cors');
-
-let databaseConnection = "Waiting for Database response...";
+const express = require("express");
+const cors = require("cors");
 
 const PORT = process.env.PORT || 8080;
-const HOST = '0.0.0.0';
+const HOST = "0.0.0.0";
 
 // App
 const app = express();
 app.use(cors());
-app.options('*', cors());
+app.options("*", cors());
 
-mongoose.connection.on("error", error => {
-    console.log("Database connection error:", error);
-    databaseConnection = "Error connecting to Database";
-});
-
-mongoose.connection.once("open", () => {
-    console.log("Connected to Database!");
-    databaseConnection = "Connected to Database";
-});
+var projectRouter = require("./routers/projects");
+app.use("/projects", projectRouter);
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
---
+++ backend/src/routers/projects.js
@@ -0,0 +1,25 @@
+const Pool = require("pg").Pool;
+var express = require("express");
+var router = express.Router();
+
+const pool = new Pool({
+  user: "postgres",
+  host: "db",
+  database: "personal_site",
+  password: "postgres",
+  port: 5432,
+});
+
+router.get("/all", function(request, response) {
+  pool.query("SELECT * FROM projects", (err, res) => {
+    if (err) {
+      console.log(err);
+      response.json(err);
+    } else {
+      console.log(res.rows);
+      response.json(res.rows);
+    }
+  });
+});
+
+module.exports = router;
