2e9b8ecd7d1671ab1cefc34a3192078748206cb1
Conor Grocock <grocock.conor@gmail.com>
2019-10-09 19:09:53 +0100

    Created simple assets server

---
+++ assets/src/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+
+const PORT = process.env.PORT || 8080;
+const HOST = "0.0.0.0";
+
+// App
+const app = express();
+app.use(cors());
+app.options("*", cors());
+
+app.use((req, res, next) => {
+  console.log("[" + new Date() + "] " + req.url);
+  next();
+});
+
+app.use(express.static("public"));
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
