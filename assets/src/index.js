c29d25ea3109c49871cdb54a0c54b8c972275109
Conor Grocock <grocock.conor@gmail.com>
2019-10-10 12:28:34 +0100

    Optimised images

--- assets/src/index.js
+++ assets/src/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const cors = require("cors");
 
+const fs = require("fs");
+var im = require("imagemagick");
+
 const PORT = process.env.PORT || 8080;
 const HOST = "0.0.0.0";
 
@@ -14,7 +17,44 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(express.static("public"));
+//app.use(express.static("public"));
+
+function resize(path, format, width, height) {
+  // const readStream = fs.createReadStream(path);
+  // let transform = sharp();
+
+  // if (format) {
+  //   transform = transform.toFormat(format);
+  // }
+
+  // if (width || height) {
+  //   transform = transform.resize(width, height);
+  // }
+
+  return readStream.pipe(transform);
+}
+
+server.get("/:filename", (req, res) => {
+  // Extract the query-parameter
+  const widthString = req.query.width;
+  const heightString = req.query.height;
+  const format = req.query.format;
+  const filename = req.params.filename;
+
+  // Parse to integer if possible
+  let width, height;
+  if (widthString) {
+    width = parseInt(widthString);
+  }
+  if (heightString) {
+    height = parseInt(heightString);
+  }
+  // Set the content-type of the response
+  res.type(`image/${format || "png"}`);
+
+  // Get the resized image
+  resize("public/" + filename, format, width, height).pipe(res);
+});
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
